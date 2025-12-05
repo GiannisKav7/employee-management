9f3c926da81240376bcd6dea74a54205e01bc9e9
GiannisKav7 <58071478+GiannisKav7@users.noreply.github.com>
2025-12-06 01:51:10 +0200

    Remove apiKey slice from Redux store
    Deleted the apiKeySlice and its reducer from the Redux store configuration. This change removes API key state management from the application, likely because it is no longer needed or has been refactored elsewhere.

--- employee-management/src/state/store.ts
+++ employee-management/src/state/store.ts
@@ -2,14 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import searchTextReducer from "./search/searchTextSlice";
 import employeesListReducer from "./employees/employeesListSlice";
 import filterDeptReducer from "./filter/filterDeptSlice";
-import apiKeyReducer from "./apiKey/apiKeySlice";
 
 export const store = configureStore({
   reducer: {
     searchText: searchTextReducer,
     employeesList: employeesListReducer,
     filterDept: filterDeptReducer,
-    apiKey: apiKeyReducer,
   },
 });
 
